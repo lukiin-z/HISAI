b0daa80e88fc92ff5a8adbc51a662492b40bcbf9
lukiin-z <lucasbaraldi2810@gmail.com>
2025-06-17 12:37:52 -0300

    app changed

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 
 app = Flask(__name__)
 
-API_KEY = 'AIzaSyDlWL4Vu8nziDGKq5PBkSUy9zvMrDjngD0'
+API_KEY = 'AIzaSyBzEyajtT9LSoBgF9BI_ui6bPK4R-ICmKU'
 
 # Função para buscar o place_id e outros detalhes via Google Places API
 def buscar_empresa_google(nome_empresa):
